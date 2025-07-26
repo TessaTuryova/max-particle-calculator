694dd537a8e2c7548285e61d6eae7f2d15065e8e
Terezia Turyova <tereturyturka@gmail.com>
2025-07-26 12:58:42 +0200

    added footer & header

---
+++ src/Footer.tsx
@@ -0,0 +1,6 @@
+function Footer() {
+    return (
+        <p>Author: Arti</p>
+    );
+}
+export default Footer
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            <h1>Max Particle Calculator</h1>
+            <a href="#">Explanation</a>
+        </header>
+    );
+}
+export default Header
